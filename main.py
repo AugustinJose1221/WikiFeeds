9975f7aa63a37ce1c8e279ef7be8d5207b255361
AugustinJose1221 <augustinjose1221@gmail.com>
2020-05-12 18:13:59 +0530

    Updated main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+url = 'https://en.wikipedia.org/wiki/Portal:Current_events'
+response = requests.get(url)
+page = response.content
+soup = BeautifulSoup(page, 'html.parser')
+text = soup.find_all(text=True)
+text1 = ""
+start = 0
+end = 0
+flag=0
+for i in range(86,len(text)):
+
+    if text[i]=="Portal":
+        if flag==1:
+            continue
+        start=i
+        flag = 1
+    if text[i]=="Recent deaths":
+        end = i
+        break
+
+text1 = text[(start+1):end]
+t = ""
+for i in text1:
+    t += i
+x = t.split("\n")
+list = []
+for j in x:
+    if j!='':
+        list.append(j)
+for i in range(1, len(list)):
+    print(list[i])
